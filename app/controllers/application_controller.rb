1d63c30e4ddf2844cc287cc31e17d73b8f9be0a6
wu-tang <ok.names.ugaman@gmail.com>
2017-08-10 11:56:16 +0900

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+   def hello
+    render html: "わっしょい！！¡¡¡"
+  end
+
+    def goodbye
+    render html: "ハプニングバー"
+  end
+end
